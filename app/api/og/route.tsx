bc7c55d008e30aa1d2ea94afe36aace2d10d1610
Tamuda <tchimhan@u.rochester.edu>
2025-11-01 13:48:38 -0400

    Update invite OG image to use new thumbnail.png with 'Ola {Name}' text in Times New Roman

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -6,12 +6,7 @@ export const runtime = "edge";
 export async function GET(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url);
-    const name = searchParams.get("name") || "WELCOME";
-
-    // Fetch the base image
-    const baseImageUrl = new URL("/thumbnail-04.png", req.url).toString();
-    const imageData = await fetch(baseImageUrl).then((res) => res.arrayBuffer());
-    const base64Image = Buffer.from(imageData).toString('base64');
+    const name = searchParams.get("name") || "Tamuda";
 
     return new ImageResponse(
       (
@@ -21,59 +16,51 @@ export async function GET(req: NextRequest) {
             width: "100%",
             display: "flex",
             position: "relative",
+            backgroundImage: "url(https://taelor-demo.vercel.app/thumbnail.png)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         >
-          {/* Base image */}
-          <img
-            src={`data:image/png;base64,${base64Image}`}
-            alt="Background"
-            style={{
-              position: "absolute",
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-            }}
-          />
-
-          {/* Semi-transparent overlay for text readability */}
-          <div
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              background: "linear-gradient(to bottom, rgba(0,0,0,0.2), transparent 40%)",
-              display: "flex",
-            }}
-          />
-
-          {/* Name at top center */}
+          {/* Text content */}
           <div
             style={{
               display: "flex",
+              flexDirection: "column",
               position: "absolute",
-              top: 40,
-              left: 0,
-              right: 0,
-              justifyContent: "center",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
               alignItems: "center",
+              justifyContent: "center",
             }}
           >
+            {/* "Ola {Name}" */}
             <div
               style={{
-                fontSize: 90,
-                fontWeight: 900,
-                color: "#d4af37",
+                fontFamily: "Times New Roman, serif",
+                fontSize: 80,
+                fontWeight: 700,
+                color: "white",
                 textAlign: "center",
-                textTransform: "uppercase",
-                letterSpacing: "0.15em",
-                textShadow: "0 4px 12px rgba(0,0,0,0.8), 0 2px 4px rgba(0,0,0,0.6)",
                 display: "flex",
-                padding: "20px 40px",
+                marginBottom: 20,
               }}
             >
-              {name}
+              Ola {name}
+            </div>
+            
+            {/* "Your winter upgrade is here." */}
+            <div
+              style={{
+                fontFamily: "Times New Roman, serif",
+                fontSize: 36,
+                fontWeight: 400,
+                color: "white",
+                textAlign: "center",
+                display: "flex",
+              }}
+            >
+              Your winter upgrade is here.
             </div>
           </div>
         </div>
